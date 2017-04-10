e144d7d0d8d5aa1137e9744c847c1e7fc40878bb
Serkan Aysan <serkan.aysan@outlook.com>
2017-04-11 00:10:44 +0300

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,15 @@ sudo apt-get install nodejs
 sudo apt-get install build-essential
 sh nodesource_setup.sh
 apt-get install nodejs
+sudo npm install -g pm2
+cd /home/ubuntu/git/node-server
+pm2 start hello.js
+pm2 startup systemd
+sudo su -c "env PATH=$PATH:/usr/bin pm2 startup systemd -u ubuntu --hp /home/ubuntu"
+systemctl status pm2
+pm2 list
+#install nginx
+sudo apt-get install nginx
+cp -i /nginx/sites-available/default /etc/nginx/sites-available/default
+nginx -t
+systemctl restart nginx
