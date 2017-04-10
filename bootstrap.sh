12c0d6f54182499ad55bc230e1972857042340c9
Serkan Aysan <serkan.aysan@outlook.com>
2017-04-10 23:35:23 +0300

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@ curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo apt-get install nodejs
 sudo apt-get install build-essential
 sh nodesource_setup.sh
+apt-get install nodejs
