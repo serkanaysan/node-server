17dc1224e7b7943d8938fe30c271393b96625d1d
Serkan Aysan <serkan.aysan@outlook.com>
2017-04-11 01:02:33 +0300

    add y

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-sudo apt-get install nodejs
-sudo apt-get install build-essential
+sudo apt-get -y install nodejs -y
+sudo apt-get -y install build-essential
 sh nodesource_setup.sh
-apt-get install nodejs
+apt-get -y install nodejs
 sudo npm install -g pm2
 cd /home/ubuntu/git/node-server
 pm2 start hello.js
@@ -11,7 +11,7 @@ sudo su -c "env PATH=$PATH:/usr/bin pm2 startup systemd -u ubuntu --hp /home/ubu
 systemctl status pm2
 pm2 list
 #install nginx
-sudo apt-get install nginx
-yes|cp -rf ./nginx/sites-available/default /etc/nginx/sites-available/default 
+sudo apt-get -y install nginx
+yes|cp -rf ./nginx/sites-available/default /etc/nginx/sites-available/default
 nginx -t
 systemctl restart nginx
